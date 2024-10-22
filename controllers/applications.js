a3d6be70d92bf0147e432c422193105c6c96476a
tntyousif <yousif02bh@gmail.com>
2024-10-22 16:35:48 +0300

    finsh delete

--- controllers/applications.js
+++ controllers/applications.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
+const session = require('express-session');
 const User =require ('../models/user.js');
 
 router.get('/', async (req, res) => {
@@ -57,6 +58,26 @@ router.get('/:applicationId', async (req, res) => {
     res.render('applications/show.ejs', {
       application: application,
     });
+  } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/')
+  }
+});
+
+// controllers/applications.js
+
+router.delete('/:applicationId', async (req, res) => {
+  try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Use the Mongoose .deleteOne() method to delete 
+    // an application using the id supplied from req.params
+    currentUser.applications.id(req.params.applicationId).deleteOne();
+    // Save changes to the user
+    await currentUser.save();
+    // Redirect back to the applications index view
+    res.redirect(`/users/${currentUser._id}/applications`);
   } catch (error) {
     // If any errors, log them and redirect back home
     console.log(error);
